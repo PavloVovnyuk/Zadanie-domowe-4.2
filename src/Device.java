8a67f071867eba4dfea4278fe9fbdcc4d84edc53
Pavlo <pawel.wowniuk@gmail.com>
2019-01-21 15:20:43 +0100

    Zadanie 4.2 (Zrobiłem wywołanie jednego konstruktora z drugiego

--- src/Device.java
+++ src/Device.java
@@ -5,9 +5,7 @@ public class Device {
     double prise;
 
     Device(String code, String producer, String type, double prise) {
-        this.code = code;
-        this.producer = producer;
-        this.type = type;
+        this(code, producer,type);
         this.prise = prise;
     }
 
